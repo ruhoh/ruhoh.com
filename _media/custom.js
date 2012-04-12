8cb068ba555d8048bc45d0431fb35accbf9a52dd
Jade Dominguez <plusjade@gmail.com>
2012-04-12 16:25:42 -0700

    Add link to log.txt file
    Also fix erroneous link to username.github.com

--- _media/custom.js
+++ _media/custom.js
@@ -66,14 +66,16 @@ var Pc = {
     var $code = $("#install-code-wrapper").find("span.typ");
     var $repoName = $("#repo_name");
     var $blogLink = $("#blog_link");
+    var $blogLogLink = $("#blog_log_link");
     $("#generate_code").submit(function(e){
       var username = $("#github_username").val();
       $code.text(username);
       $repoName.val( username + ".ruhoh.com");
-      $blogLink.text("http://" + username + ".ruhoh.com").attr("href", "http://" + username + ".github.com");
+      var link = "http://" + username + ".ruhoh.com";
+      $blogLink.text(link).attr("href", link);
+      $blogLogLink.attr("href", link + '/log.txt');
       if ($.trim(username) !== ""){ 
         //if (typeof mpq !== 'undefined') mpq.track("install", {"username": username });
-        //if (typeof _gaq !== 'undefined') _gaq.push(['_trackEvent', 'Forms', 'Input', username]);
       }  
       e.preventDefault();
       return false;
